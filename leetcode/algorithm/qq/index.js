a4452d4cd14171cd0411b388589c58853d59ea13
pppmmmqqq <2674264426@qq.com>
2020-02-16 13:08:27 +0800

    QQ算法

--- leetcode/algorithm/qq/index.js
+++ leetcode/algorithm/qq/index.js
@@ -20,3 +20,22 @@ const is_valid_qq = (qq) => {
     return true;
 };
 console.log(is_valid_qq(qq_number));
+
+//选择了一种数据结构 数组
+let enc_qq = [6,3,1,7,5,8,9,2,4],
+ qq = [],
+ head = 0, //指针 start 开始
+ tail = 9; //尾部 end 指针
+
+
+//  拿到一个正确的数字，操作原来的两个数字
+ while(head < tail) {  // 得到一个正确数字
+    qq.push(enc_qq[head]);
+    head++;
+    //  第二个数移到尾部
+    enc_qq[tail] = enc_qq[head]
+    //  删除它？
+    tail++;
+    head++;
+ }
+ console.log(qq);
