61c63ddd49e63388efd866cba57ab28559d0f342
pppmmmqqq <2674264426@qq.com>
2020-03-13 11:09:30 +0800

    vuex再理解

--- vue/vuex-mongo-user/src/api/index.js
+++ vue/vuex-mongo-user/src/api/index.js
@@ -1,60 +1,74 @@
-// 数据驱动页面
-const _users = [
-  {
-    "address": {
-      "city": "Los Angeles",
-      "state": 'California',
-      "pincode": "123"
-    },
-    "tags": [
-      "music",
-      "blogs",
-      "cricket"
-    ],
-    "name": "Tom Benzamin"
-  },
-  {
-    "address": {
-      "city": "景德镇",
-      "state": '江西',
-      "pincode": "330000"
-    },
-    "tags": [
-      "coding",
-      "blogs"
-    ],
-    "name": "xiaop"
-  },
-  {
-    "address": {
-      "city": "武汉",
-      "state": '湖北',
-      "pincode": "331000"
-    },
-    "tags": [
-      "coding",
-      "swim"
-    ],
-    "name": "dap"
-  },
-    {
-    "address": {
-      "city": "芜湖",
-      "state": '安徽',
-      "pincode": "331000"
-    },
-    "tags": [
-      "coding",
-      "games"
-    ],
-    "name": "xiaow"
-  }
-]
-// 默认输出 cb 传入的参数
+// const _users = [
+//   {
+//     "address": {
+//       "city": "Los Angeles",
+//       "state": 'California',
+//       "pincode": "123"
+//     },
+//     "tags": [
+//       "music",
+//       "blogs",
+//       "cricket"
+//     ],
+//     "name": "Tom Benzamin"
+//   },
+//   {
+//     "address": {
+//       "city": "赣州",
+//       "state": '江西',
+//       "pincode": "331000"
+//     },
+//     "tags": [
+//       "coding",
+//       "blogs"
+//     ],
+//     "name": "王志浩"
+//   },
+//   {
+//     "address": {
+//       "city": "九江",
+//       "state": '江西',
+//       "pincode": "331000"
+//     },
+//     "tags": [
+//       "coding",
+//       "swim"
+//     ],
+//     "name": "刘子民"
+//   },
+//     {
+//     "address": {
+//       "city": "赣州",
+//       "state": '江西',
+//       "pincode": "331000"
+//     },
+//     "tags": [
+//       "coding",
+//       "games"
+//     ],
+//     "name": "衷从海"
+//   }
+// ]
 export default {
   fetchUsers(cb) {
-    setTimeout(() => {
-      cb(_users);
-    }, 1000)
+    // setTimeout(() => {
+    //   cb(_users);
+    // }, 1000)
+    fetch('/api/users')
+      .then(data => data.json())
+      .then(data => {
+        console.log(data, '----api');
+        cb(data)
+      })
+      .catch(err => {
+        cb(err)
+      })
+  },
+  fetchUsersByTag(tag, cb) {
+    fetch(`/api/users/tag/${tag}`)
+    .then(data => data.json())
+    .then(data => {
+      cb(data)
+    })
   }
 }
