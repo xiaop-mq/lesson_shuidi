c4ce9fb73e6fe02fd3a6025f9d2e1fb67518f57f
pppmmmqqq <2674264426@qq.com>
2020-04-08 21:23:27 +0800

    react关于jsx和state和组件入门

---
+++ react/react-base/src/setupTests.js
@@ -0,0 +1,6 @@
+// 测试 软件开发很重要的环节
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// allows you to do things like:
+// expect(element).toHaveTextContent(/react/i)
+// learn more: https://github.com/testing-library/jest-dom
+import '@testing-library/jest-dom/extend-expect';
