49ea511ec0f0ca08e8634867f670df7d1779f969
pppmmmqqq <2674264426@qq.com>
2020-04-25 19:54:18 +0800

    redux入门学习

---
+++ Redux-Demo/demo01/src/TodoList.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import 'antd/dist/antd.css'
+import { Input , Button , List } from 'antd'
+import store from './store'
+
+
+
+class TodoList extends Component {
+    constructor(props){
+        super(props)
+        this.state=store.getState();
+        this.changeInputValue= this.changeInputValue.bind(this)
+        //----------关键代码-----------start
+        this.storeChange = this.storeChange.bind(this)  //转变this指向
+        store.subscribe(this.storeChange) //订阅Redux的状态
+        //----------关键代码-----------end
+    }
+    render() { 
+        return ( 
+            <div style={{margin:'10px'}}>
+                <div>
+                    <Input 
+                        placeholder={this.state.inputValue} 
+                        style={{ width:'250px', marginRight:'10px'}}
+                        onChange={this.changeInputValue}
+                    />
+                    <Button type="primary">增加</Button>
+                </div>
+                <div style={{margin:'10px',width:'300px'}}>
+                    <List
+                        bordered
+                        dataSource={this.state.list}
+                        renderItem={item=>(<List.Item>{item}</List.Item>)}
+                    />    
+                </div>
+            </div>
+         );
+    }
+    changeInputValue(e){
+        const action ={
+            type:'changeInput',
+            value:e.target.value
+        }
+        store.dispatch(action)
+    }
+    storeChange(){
+        this.setState(store.getState())
+    }
+}
+export default TodoList;
---
+++ Redux-Demo/demo01/src/store/reducer.js
@@ -0,0 +1,16 @@
+const defaultState = {
+    inputValue:'Write Somthing',
+    list:[
+        '早8点开晨会，分配今天的开发工作',
+        '早9点和项目经理作开发需求讨论会',
+        '晚5:30对今日代码进行review'
+    ]
+}
+export default (state = defaultState,action)=>{
+    if(action.type === 'changeInput'){
+        let newState = JSON.parse(JSON.stringify(state)) //深度拷贝state
+        newState.inputValue = action.value
+        return newState
+    }
+    return state
+}
