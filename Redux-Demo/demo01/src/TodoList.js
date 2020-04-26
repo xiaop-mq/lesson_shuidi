f1375b0c84b024ae142bcc83727b62edd01b3f43
pppmmmqqq <2674264426@qq.com>
2020-04-26 10:45:49 +0800

    redux入门学习

--- Redux-Demo/demo01/src/TodoList.js
+++ Redux-Demo/demo01/src/TodoList.js
@@ -2,19 +2,19 @@ import React, { Component } from 'react';
 import 'antd/dist/antd.css'
 import { Input , Button , List } from 'antd'
 import store from './store'
-
-
+//关键代码-------------start
+import {changeInputAction , addItemAction ,deleteItemAction} from './store/actionCreatores'
+//关键代码------------end
 
 class TodoList extends Component {
-    constructor(props){
-        super(props)
-        this.state=store.getState();
-        this.changeInputValue= this.changeInputValue.bind(this)
-        //----------关键代码-----------start
-        this.storeChange = this.storeChange.bind(this)  //转变this指向
-        store.subscribe(this.storeChange) //订阅Redux的状态
-        //----------关键代码-----------end
-    }
+constructor(props){
+    super(props)
+    this.state=store.getState();
+    this.changeInputValue= this.changeInputValue.bind(this)
+    this.storeChange = this.storeChange.bind(this)
+    this.clickBtn = this.clickBtn.bind(this)
+    store.subscribe(this.storeChange) //订阅Redux的状态
+}
     render() { 
         return ( 
             <div style={{margin:'10px'}}>
@@ -23,28 +23,40 @@ class TodoList extends Component {
                         placeholder={this.state.inputValue} 
                         style={{ width:'250px', marginRight:'10px'}}
                         onChange={this.changeInputValue}
+                        value={this.state.inputValue}
                     />
-                    <Button type="primary">增加</Button>
+                    <Button 
+                        type="primary"
+                        onClick={this.clickBtn}
+                    >增加</Button>
                 </div>
                 <div style={{margin:'10px',width:'300px'}}>
                     <List
                         bordered
                         dataSource={this.state.list}
-                        renderItem={item=>(<List.Item>{item}</List.Item>)}
+                        renderItem={(item,index)=>(<List.Item onClick={this.deleteItem.bind(this,index)}>{item}</List.Item>)}
                     />    
                 </div>
             </div>
          );
     }
-    changeInputValue(e){
-        const action ={
-            type:'changeInput',
-            value:e.target.value
-        }
-        store.dispatch(action)
-    }
     storeChange(){
+        console.log('store changed')
         this.setState(store.getState())
     }
+    //--------关键代码------start
+    changeInputValue(e){
+        const action = changeInputAction(e.target.value)
+        store.dispatch(action)
+    }
+    clickBtn(){
+        const action = addItemAction()
+        store.dispatch(action)
+    }
+    deleteItem(index){
+        const action = deleteItemAction(index)
+        store.dispatch(action)
+    }
+    //--------关键代码------end
 }
 export default TodoList;
--- Redux-Demo/demo01/src/store/reducer.js
+++ Redux-Demo/demo01/src/store/reducer.js
@@ -1,3 +1,5 @@
+import {CHANGE_INPUT,ADD_ITEM,DELETE_ITEM} from './actionTypes'
+
 const defaultState = {
     inputValue:'Write Somthing',
     list:[
@@ -7,10 +9,26 @@ const defaultState = {
     ]
 }
 export default (state = defaultState,action)=>{
-    if(action.type === 'changeInput'){
+    if(action.type === CHANGE_INPUT){
         let newState = JSON.parse(JSON.stringify(state)) //深度拷贝state
         newState.inputValue = action.value
         return newState
     }
+    //关键代码------------------start----------
+    //state值只能传递，不能使用
+    if(action.type === ADD_ITEM ){ //根据type值，编写业务逻辑
+        let newState = JSON.parse(JSON.stringify(state)) 
+        newState.list.push(newState.inputValue)  //push新的内容到列表中去
+        newState.inputValue = ''
+        return newState
+    }
+
+    if(action.type === DELETE_ITEM ){ //根据type值，编写业务逻辑
+        let newState = JSON.parse(JSON.stringify(state)) 
+        newState.list.splice(action.index,1)  //push新的内容到列表中去
+        newState.inputValue = ''
+        return newState
+    }
+     //关键代码------------------end----------
     return state
 }
