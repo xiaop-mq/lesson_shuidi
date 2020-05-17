06c41cb9284a714fcd7b0e4555c73789c8628534
pppmmmqqq <2674264426@qq.com>
2020-05-17 16:04:18 +0800

    concurrent特性

---
+++ react/react-expriment/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import logo from './logo.svg';
+import ConcurrentDemo from './ConcurrentDemo'
+import './App.css';
+
+function App() {
+  return (
+    <ConcurrentDemo />
+  );
+}
+
+export default App;
