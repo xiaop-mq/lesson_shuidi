10dcf5f93832c736960582fc18932c1c4490dab2
pppmmmqqq <2674264426@qq.com>
2020-05-21 09:47:25 +0800

    map学习

---
+++ js/Map/1.js
@@ -0,0 +1,22 @@
+let myMap = new Map();
+ 
+let keyObj = {};
+let keyFunc = function() {};
+let keyString = 'a string';
+ 
+// 添加键
+myMap.set(keyString, "和键'a string'关联的值");
+myMap.set(keyObj, "和键keyObj关联的值");
+myMap.set(keyFunc, "和键keyFunc关联的值");
+ 
+myMap.size; // 3
+ 
+// 读取值
+myMap.get(keyString);    // "和键'a string'关联的值"
+myMap.get(keyObj);       // "和键keyObj关联的值"
+myMap.get(keyFunc);      // "和键keyFunc关联的值"
+ 
+myMap.get('a string');   // "和键'a string'关联的值"
+                         // 因为keyString === 'a string'
+myMap.get({});           // undefined, 因为keyObj !== {}
+myMap.get(function() {}); // undefined, 因为keyFunc !== function () {}
